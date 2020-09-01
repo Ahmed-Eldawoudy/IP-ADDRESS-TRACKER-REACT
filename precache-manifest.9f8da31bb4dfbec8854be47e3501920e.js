3778fc5561c3e385fa20eb524aa627c427a4697f
Ahmed-Eldawoody <60572865+Ahmed-Eldawoody@users.noreply.github.com>
2020-09-01 23:16:32 +0200

    Updates

---
+++ precache-manifest.9f8da31bb4dfbec8854be47e3501920e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1358f4d2a037f4efda1a64f15fc4bcf4",
+    "url": "/Ahmed-Eldawoody/IP-ADDRESS-TRACKER.git/index.html"
+  },
+  {
+    "revision": "42ba90e9548a5916ba91",
+    "url": "/Ahmed-Eldawoody/IP-ADDRESS-TRACKER.git/static/css/2.067ccc96.chunk.css"
+  },
+  {
+    "revision": "ab7bff9bbda8b98b2b45",
+    "url": "/Ahmed-Eldawoody/IP-ADDRESS-TRACKER.git/static/css/main.5cd8e28d.chunk.css"
+  },
+  {
+    "revision": "42ba90e9548a5916ba91",
+    "url": "/Ahmed-Eldawoody/IP-ADDRESS-TRACKER.git/static/js/2.2a2c10dd.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/Ahmed-Eldawoody/IP-ADDRESS-TRACKER.git/static/js/2.2a2c10dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab7bff9bbda8b98b2b45",
+    "url": "/Ahmed-Eldawoody/IP-ADDRESS-TRACKER.git/static/js/main.6a3d9441.chunk.js"
+  },
+  {
+    "revision": "d5712ada147750152c72",
+    "url": "/Ahmed-Eldawoody/IP-ADDRESS-TRACKER.git/static/js/runtime-main.2bc6e94f.js"
+  },
+  {
+    "revision": "53ef8ed8ef4b92721f00cb8bfdc5c9b9",
+    "url": "/Ahmed-Eldawoody/IP-ADDRESS-TRACKER.git/static/media/icon-arrow.53ef8ed8.svg"
+  }
+]);
